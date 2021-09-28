d49a0d7dbd07bbc675682658f841fd2ccb314060
Andres Genes <andresgenes@hotmai.com>
2021-09-28 16:46:27 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const hbs = require('hbs');
+const express = require('express');
+const app = express();
+
+
+const viewsPath = path.join(__dirname, 'templates/views');
+const partialsPath = path.join(__dirname, 'templates/partials');
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.get('', (req, res) => {
+  res.render('index');
+});
+
+app.get('/us', (req, res) => {
+  res.render('us');
+});
+
+app.get('/cookiespolicy', (req, res) => {
+  res.render('cookiespolicy');
+});
+
+app.get('/privacypolicy', (req, res) => {
+  res.render('privacypolicy');
+});
+
+app.get('/legalwarning', (req, res) => {
+  res.render('legalwarning');
+});
+
+app.listen(3000, () => { console.log('Conexion exitosa'); });
+
---
+++ public/js/shoes.js
@@ -0,0 +1,11 @@
+VanillaTilt.init(document.querySelectorAll(".boxsh"), {
+  max: 25,
+  speed: 400
+});
+
+VanillaTilt.init(document.querySelectorAll(".boxsh"), {
+  max: 25,
+  speed: 400
+});
+
+VanillaTilt.init(document.querySelectorAll(".your-element"));
